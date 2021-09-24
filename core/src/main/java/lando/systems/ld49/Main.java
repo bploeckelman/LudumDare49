a7c7b2d20c36768c04bc8d868b1a32a44c87c7c8
Doug Graham <dsgraham@gmail.com>
2021-09-24 14:11:54 -0500

    Making another change to test out the build process

--- core/src/main/java/lando/systems/ld49/Main.java
+++ core/src/main/java/lando/systems/ld49/Main.java
@@ -76,7 +76,7 @@ public class Main extends ApplicationAdapter {
     public void render() {
         update();
 
-        ScreenUtils.clear(Color.YELLOW);
+        ScreenUtils.clear(Color.RED);
         batch.begin();
         {
             batch.draw(texture, 0, 0, Config.window_width, Config.window_height);

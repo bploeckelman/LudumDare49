0cc2ab24543e5095b4b1d47175f1ba8ea7755362
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-10-01 19:31:55 -0500

    Update title - Banana Republic: Nuclear Power Plantain

--- core/src/main/java/lando/systems/ld49/Config.java
+++ core/src/main/java/lando/systems/ld49/Config.java
@@ -1,7 +1,7 @@
 package lando.systems.ld49;
 
 public class Config {
-    public static final String title = "Ludum Dare 49";
+    public static final String title = "Banana Republic: Nuclear Power Plantain";
     public static final int window_width = 1280;
     public static final int window_height = 720;
     public static final int viewport_width = window_width / 2;

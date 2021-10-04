1b1aad5a2b292dd8287a9379832240351a841ee3
Doug Graham <dsgraham@wisc.edu>
2021-10-04 12:51:59 -0500

    tutorial Stubs

--- core/src/main/java/lando/systems/ld49/world/Tutorial.java
+++ core/src/main/java/lando/systems/ld49/world/Tutorial.java
@@ -43,9 +43,9 @@ public class Tutorial {
         }
         if (currentTutorialItem != null) {
             if (targetAlpha > alpha) {
-                alpha = MathUtils.clamp(alpha + dt, 0, maxAlpha);
+                alpha = MathUtils.clamp(alpha + 1.5f * dt, 0, maxAlpha);
             } else if (targetAlpha < alpha) {
-                alpha = MathUtils.clamp(alpha - dt, 0, maxAlpha);
+                alpha = MathUtils.clamp(alpha - 3f * dt, 0, maxAlpha);
             }
             if (alpha == maxAlpha) {
                 // Ready to draw
@@ -67,8 +67,12 @@ public class Tutorial {
 
     public void initialize(){
         initialized = true;
-        activeItems.add(new TutorialItem(Main.game.assets.strings.get("introText"), new Rectangle(0, 0, 225, 136), new Rectangle(200, 400, 800, 300)));
+        activeItems.add(new TutorialItem(Main.game.assets.strings.get("introText"), new Rectangle(100000, 100000, 0, 0), new Rectangle(200, 400, 400, 300)));
+        activeItems.add(new TutorialItem(Main.game.assets.strings.get("reactorText"), new Rectangle(459, 98, 687, 483), new Rectangle(50, 200, 400, 500)));
+        activeItems.add(new TutorialItem(Main.game.assets.strings.get("heatText"), new Rectangle(0, 0, 225, 136), new Rectangle(200, 400, 800, 300)));
         activeItems.add(new TutorialItem(Main.game.assets.strings.get("structureText"), new Rectangle(1059, 0, 225, 136), new Rectangle(200, 400, 800, 300)));
+        activeItems.add(new TutorialItem(Main.game.assets.strings.get("pistonText"), new Rectangle(636, 100, 350, 50), new Rectangle(200, 400, 800, 300)));
+        activeItems.add(new TutorialItem(Main.game.assets.strings.get("slingText"), new Rectangle(215, 184, 130, 70), new Rectangle(200, 400, 800, 300)));
 
 
     }
@@ -78,7 +82,7 @@ public class Tutorial {
             TutorialItem item = currentTutorialItem;
             TextureRegion pixel = Main.game.assets.pixelRegion;
 
-            batch.setColor(.2f, .2f, .2f, alpha*.9f);
+            batch.setColor(.1f, .1f, .1f, alpha*.9f);
             // Draw shaded area outlining the area of interest
             batch.draw(pixel, 0, 0, item.bounds.x, item.bounds.y);
             batch.draw(pixel, 0, item.bounds.y, item.bounds.x, item.bounds.height);
@@ -91,6 +95,9 @@ public class Tutorial {
             batch.draw(pixel, item.bounds.x + item.bounds.width, item.bounds.y, camera.viewportWidth, item.bounds.height);
             batch.draw(pixel, item.bounds.x + item.bounds.width, item.bounds.y + item.bounds.height, camera.viewportWidth, camera.viewportHeight);
 
+            batch.setColor(1,1,0, Math.abs(MathUtils.sin(accum * 3f) * alpha));
+            Main.game.assets.debugNinePatch.draw(batch, item.bounds.x, item.bounds.y, item.bounds.width, item.bounds.height);
+
             batch.setColor(Color.WHITE);
             // Draw the text
             batch.setColor(0, .5f, .5f, alpha * alpha);
@@ -98,9 +105,11 @@ public class Tutorial {
             batch.setColor(Color.WHITE);
             item.typingLabel.render(batch, alpha);
             if (item.typingLabel.hasEnded()){
+                Main.game.assets.pixelFont16.getData().setScale(.5f);
                 Main.game.assets.layout.setText(Main.game.assets.pixelFont16, "Click to Continue", Color.WHITE, 200, Align.left, false);
                 Main.game.assets.pixelFont16.setColor(1, 1, 1, alpha);
                 Main.game.assets.pixelFont16.draw(batch, "Click to Continue", item.textBounds.x + item.textBounds.width - Main.game.assets.layout.width, item.textBounds.y + 5 + Main.game.assets.layout.height);
+                Main.game.assets.pixelFont16.getData().setScale(1f);
             }
         }
 
